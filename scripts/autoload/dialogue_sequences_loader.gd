749dcac6d6b867b8149225c5aae9a05d8531808a
Limofeus <69484438+Limofeus@users.noreply.github.com>
2025-06-17 02:16:40 +0300

    СПК

--- scripts/autoload/dialogue_sequences_loader.gd
+++ scripts/autoload/dialogue_sequences_loader.gd
@@ -4,5 +4,22 @@ class_name DialogueSequencesLoader #way to localize dialogue sequences
 @export var dialogue_sequence_folder : String = "res://resources/dialogue sequences/"
 var dialogue_sequences : Dictionary = {}
 
+func _ready():
+	prepare_dialogue_sequences("en")
+	DialogueSequencesLoaderInstance.log_dialogue_sequences()
+
 func prepare_dialogue_sequences(language_key : String) -> void:
 	dialogue_sequences = {}
+	var dialogue_resource_paths := StaticUtility.get_all_file_paths(dialogue_sequence_folder)
+	for dialogue_resource_path in dialogue_resource_paths:
+		parse_dialogue_sequence(dialogue_resource_path, language_key)
+
+func parse_dialogue_sequence(dialogue_resource_path : String, language_key : String) -> void:
+	var dialogue_resource : DialogueSequence = load(dialogue_resource_path)
+	if dialogue_resource.sequence_lang_key == language_key:
+		dialogue_sequences[dialogue_resource.sequence_name] = dialogue_resource
+
+func log_dialogue_sequences() -> void:
+	print("PREPARING DIALOGUE SEQUENCES...")
+	for sequence_name in dialogue_sequences.keys():
+		print("NAME: ", sequence_name, ", PATH: ", dialogue_sequences[sequence_name].resource_path)
--- scripts/utility/static_utility.gd
+++ scripts/utility/static_utility.gd
@@ -26,3 +26,17 @@ static func rotate_towards(current : Vector3, target : Vector3, rotation_speed :
 static func set_node_array_process_mode(node_array : Array[Node], new_process_mode) -> void:
 	for node in node_array:
 		node.process_mode = new_process_mode
+
+static func get_all_file_paths(path: String) -> Array[String]: #Got this somewhere from reddit, idk how well this works
+	var file_paths: Array[String] = []  
+	var dir = DirAccess.open(path)  
+	dir.list_dir_begin()  
+	var file_name = dir.get_next()  
+	while file_name != "":  
+		var file_path = path + "/" + file_name  
+		if dir.current_is_dir():  
+			file_paths += get_all_file_paths(file_path)  
+		else:  
+			file_paths.append(file_path)  
+		file_name = dir.get_next()  
+	return file_paths
