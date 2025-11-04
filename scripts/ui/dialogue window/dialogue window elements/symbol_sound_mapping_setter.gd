3360a8a10e78d21f0f455ea945a49d87fe22a96e
Limofeus <69484438+Limofeus@users.noreply.github.com>
2025-11-04 03:43:45 +0300

    Смена наборов звуков через метадату диалога

--- scripts/ui/dialogue window/dialogue window elements/symbol_sound_mapping_setter.gd
+++ scripts/ui/dialogue window/dialogue window elements/symbol_sound_mapping_setter.gd
@@ -1,9 +1,10 @@
 extends DialogueWindowElement
 
-@export var debug_ssm : DialogueSymbolSoundsMapping = null
+@export var default_symbol_sound_mapping : DialogueSymbolSoundsMapping = null
 
 func _ready():
-	dialogue_window_visual.text_processor.dialogue_symbol_sound_mapping = debug_ssm#TODO: remove!
+	dialogue_window_visual.text_processor.dialogue_symbol_sound_mapping = default_symbol_sound_mapping
+	super()
 
 func bound_entry_changed(_new_value : Variant) -> void:
 	if dialogue_window_visual != null:
