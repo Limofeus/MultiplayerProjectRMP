47c27290944d498e650e69472ab796397afa4044
Limofeus <69484438+Limofeus@users.noreply.github.com>
2025-11-09 20:41:09 +0300

    #3 магазин: purely for git streak..

--- scripts/resources/store/store listing items/add_inventory_item_listing_item.gd
+++ scripts/resources/store/store listing items/add_inventory_item_listing_item.gd
@@ -5,3 +5,10 @@ class_name AddInventoryItemListingItem
 
 var recipient_inventory : ItemContainer = null
 
+func purchase_available() -> bool:
+	return recipient_inventory.max_free_space_for_item(inventory_item) > 0
+
+func purchase_listing() -> void:
+	pass
+	#recipient_inventory.add_item(inventory_item)
+	#Cus inventory add function is probly different for players
