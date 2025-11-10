3014c87ce45edb9d9533824e38adb020b77f1eb2
Limofeus <69484438+Limofeus@users.noreply.github.com>
2025-11-10 03:39:51 +0300

    #4 магазин: наработки визуала (хотя нужно будет фулл переделать по хорошему потом хах...) всё ещё не придумал продумал архитектуру кода магаза

--- scripts/resources/store/store listing items/add_inventory_item_listing_item.gd
+++ scripts/resources/store/store listing items/add_inventory_item_listing_item.gd
@@ -2,13 +2,15 @@ extends StoreListingItem
 class_name AddInventoryItemListingItem
 
 @export var inventory_item : InventoryItem = null
+@export var item_count : int = 1
 
 var recipient_inventory : ItemContainer = null
 
+signal request_item_add(inventory_item : InventoryItem, count : int)
+
 func purchase_available() -> bool:
-	return recipient_inventory.max_free_space_for_item(inventory_item) > 0
+	return recipient_inventory.max_free_space_for_item(inventory_item) >= item_count
 
 func purchase_listing() -> void:
-	pass
-	#recipient_inventory.add_item(inventory_item)
-	#Cus inventory add function is probly different for players
+	request_item_add.emit(inventory_item, item_count)
+	#recipient_inventory.add_item(inventory_item) #Add directly
