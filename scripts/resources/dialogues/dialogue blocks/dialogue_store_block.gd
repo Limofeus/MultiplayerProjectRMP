9fcaead148450e9bec82cd019835451c3d207831
Limofeus <69484438+Limofeus@users.noreply.github.com>
2025-11-12 23:32:18 +0300

    #5 магазин: к осознанию

--- scripts/resources/dialogues/dialogue blocks/dialogue_store_block.gd
+++ scripts/resources/dialogues/dialogue blocks/dialogue_store_block.gd
@@ -3,4 +3,4 @@ extends DialogueBlock
 @export var store_preset : Store = null
 
 func block_start(_dialogue_parameters : Dictionary = {}, _sync_pass : bool = true) -> void:
-	pass
+	var store_model : Store = store_preset.duplicate()
