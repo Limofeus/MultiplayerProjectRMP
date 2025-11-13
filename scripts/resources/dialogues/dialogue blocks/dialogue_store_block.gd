b9d9f36b4bfb0028a6243dd36b2ba49b1ecfde37
Limofeus <69484438+Limofeus@users.noreply.github.com>
2025-11-13 22:08:22 +0300

    #7 магазин: отсутствия

--- scripts/resources/dialogues/dialogue blocks/dialogue_store_block.gd
+++ scripts/resources/dialogues/dialogue blocks/dialogue_store_block.gd
@@ -3,4 +3,6 @@ extends DialogueBlock
 @export var store_preset : Store = null
 
 func block_start(_dialogue_parameters : Dictionary = {}, _sync_pass : bool = true) -> void:
+	if !_sync_pass:
+		return
 	var store_model : Store = store_preset.duplicate()
