e942f1caced7804cc43ade75ad856f889dcdf5a4
Limofeus <69484438+Limofeus@users.noreply.github.com>
2025-11-16 23:11:03 +0300

    #10 магазин: прогресса

--- scripts/resources/dialogues/dialogue blocks/dialogue_store_block.gd
+++ scripts/resources/dialogues/dialogue blocks/dialogue_store_block.gd
@@ -2,7 +2,7 @@ extends DialogueBlock
 
 @export var store_preset : Store = null
 
-#TODO: get more time for this...
+#TODO: get more time for this... hope soon I'll get time ror this
 func block_start(_dialogue_parameters : Dictionary = {}, _sync_pass : bool = true) -> void:
 	if !_sync_pass:
 		return
